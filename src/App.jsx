74478eaca006b53fb9b91b96a3fe8d968d861d01
van-gomes <nninha.gomes@gmail.com>
2025-01-12 18:37:24 -0300

    Working with CSS header and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
       <Header />
-      <h1>React App</h1>
     </>
   )
 }
--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -3,9 +3,9 @@ import styles from './NavLinks.module.css';
 
 export const NavLinks = () => {
   return (
-    <nav>
+    <nav className={styles.container__nav}>
       <ul className={styles.nav__links}>
-        <li>
+        <li className={styles.nav__link__home}>
           <a href="/">
             HOME
           </a>
