7c9404660e68b1c121de5b2f8f9888bd1457fb69
van-gomes <nninha.gomes@gmail.com>
2025-01-12 19:43:10 -0300

    Create Banner Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.module.css';
+import Banner from './components/Banner';
 import { Header } from './components/Header/Header';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   )
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import banner from "../../assets/bannerMain.png";
+import styles from "./Banner.module.css";
+
+export const Banner = () => {
+  return (
+    <section className={styles.container__banner}>
+        <img src={banner} alt="Banner" />
+    </section> 
+  );
+};
