5e71cdea406ed64769993d9c4eea6bdb680f231f
van-gomes <nninha.gomes@gmail.com>
2025-01-15 21:38:50 -0300

    Test Form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,6 +5,8 @@ export const Form = () => {
     return (
         <form className={style.container__form}>
             <TextField label="Name" type="text" placeholder="Enter your name" />
+            <TextField label="description" type="text" placeholder="Enter your description" />
+            <TextField label="image" type="text" placeholder="Enter your image" />
         </form>
     );
 }
