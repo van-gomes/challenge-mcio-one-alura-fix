fb06bd37e8a2e76117eed25f4d8e0a344a498c5e
van-gomes <nninha.gomes@gmail.com>
2025-01-16 18:58:04 -0300

    Insert form content

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,10 +3,11 @@ import style from './Form.module.css';
 
 export const Form = () => {
     return (
-        <form className={style.container__form}>
+        <div className={style.content__form}>
             <TextField label="Name" type="text" placeholder="Enter your name" />
             <TextField label="description" type="text" placeholder="Enter your description" />
             <TextField label="image" type="text" placeholder="Enter your image" />
-        </form>
+            <TextField label="Video" type="text" placeholder="Enter your video" />
+        </div>
     );
 }
