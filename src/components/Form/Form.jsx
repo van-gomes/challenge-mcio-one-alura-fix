91a121d5256dfbe145f4f7899202e536f20befec
van-gomes <nninha.gomes@gmail.com>
2025-01-19 17:21:02 -0300

    Refactor Form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,17 @@
 import TextField from '../TextField';
-import style from './Form.module.css';
+import styles from './Form.module.css';
 
 export const Form = () => {
     return (
-        <div className={style.content__form}>
-            <TextField label="Name" type="text" placeholder="Enter your name" />
-            <TextField label="image" type="text" placeholder="Enter your image" />
-            <TextField label="Video" type="text" placeholder="Enter your video" />
-            <TextField label="description" type="text" placeholder="Enter your description" />
-        </div>
+        <section className={styles.form}>
+            <form>
+                <h1>NOVO VÍDEO</h1>
+                <h2>Complete o formulário para criar um novo card de vídeo.</h2>
+                <TextField label="Name" type="text" placeholder="Enter your name" />
+                <TextField label="image" type="text" placeholder="Enter your image" />
+                <TextField label="Video" type="text" placeholder="Enter your video" />
+                <TextField label="description" type="text" placeholder="Enter your description" />
+            </form>
+        </section>
     );
 }
