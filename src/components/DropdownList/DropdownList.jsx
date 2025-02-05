60aa02a4041808c9fbdeb4422ffd7b2ff63165b0
van-gomes <nninha.gomes@gmail.com>
2025-02-04 21:35:09 -0300

    Make it mobile responsive

--- src/components/DropdownList/DropdownList.jsx
+++ src/components/DropdownList/DropdownList.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
+import styles from "./DropdownList.module.css";
 
 export const DropdownList = ({ label, itens, value, aoAlterado, obrigatorio = false }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <label>{label}</label>
       <select required={obrigatorio} value={value} onChange={evento => aoAlterado(evento.target.value)}>
           <option value=""></option>
