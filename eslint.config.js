a9fb6d90fa307049d15cd9c9f66548dc74865fd0
van-gomes <nninha.gomes@gmail.com>
2025-02-15 17:15:08 -0300

    Install and configure ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,31 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from "globals";
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import babelParser from "@babel/eslint-parser";
 
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: babelParser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"]
+        }
+      }
     },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    plugins: { react },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+      "react/react-in-jsx-scope": "off",
+      "no-console": "warn",
+      "react/prop-types": "off"
+    }
+  }
+];
